c1ce02bddf6dbf2bc98499ddaef8168b906873c9
letyresinaa <leticiaresina@gmail.com>
2025-09-15 16:05:27 -0300

    feat: valores das entidades

--- src/main/java/com/br/fiap/nexora/model/Cliente.java
+++ src/main/java/com/br/fiap/nexora/model/Cliente.java
@@ -1,4 +1,26 @@
 package com.br.fiap.nexora.model;
 
+import com.br.fiap.nexora.enums.PerfilInvestidor;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Cliente {
+    @Id
+    private String cpf;
+
+    private String nome;
+    private String email;
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    private PerfilInvestidor perfilInvestidor;
+
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+
+    private String telefone;
 }
--- src/main/java/com/br/fiap/nexora/model/Endereco.java
+++ src/main/java/com/br/fiap/nexora/model/Endereco.java
@@ -1,4 +1,14 @@
 package com.br.fiap.nexora.model;
 
+import jakarta.persistence.Id;
+
 public class Endereco {
+    @Id
+    int cep;
+    String rua;
+    int numero;
+    String complemento;
+    String bairro;
+    String cidade;
+    String uf;
 }
