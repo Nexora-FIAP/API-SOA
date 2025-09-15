fdb01c96a38da2144199f650c88f0e140595e2b0
letyresinaa <leticiaresina@gmail.com>
2025-09-15 15:31:50 -0300

    feat: projeto inicial

---
+++ src/main/java/com/br/fiap/nexora/NexoraApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.nexora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraApplication.class, args);
+	}
+
+}
