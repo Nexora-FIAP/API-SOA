1e076c709897eb6dee8ac408065307950c86da23
letyresinaa <leticiaresina@gmail.com>
2025-09-20 16:03:17 -0300

    feat: endereco controller

--- src/main/java/com/br/fiap/nexora/controller/EnderecoController.java
+++ src/main/java/com/br/fiap/nexora/controller/EnderecoController.java
@@ -1,4 +1,34 @@
 package com.br.fiap.nexora.controller;
 
+import com.br.fiap.nexora.dto.EnderecoDTO;
+import com.br.fiap.nexora.model.Endereco;
+import com.br.fiap.nexora.repository.EnderecoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/endereco")
 public class EnderecoController {
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Endereco> cadastrarEndereco(@RequestBody @Valid EnderecoDTO enderecoDTO) {
+        Endereco endereco = new Endereco(enderecoDTO);
+        enderecoRepository.save(endereco);
+        return ResponseEntity.status(HttpStatus.CREATED).body(endereco);
+    }
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<Endereco> buscarEndereco(@PathVariable String cep) {
+        return enderecoRepository.findById(cep)
+                .map(endereco -> ResponseEntity.ok().body(endereco))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
