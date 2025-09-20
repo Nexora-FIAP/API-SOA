b4ac48bdc7ba9c5a69af4663895bf473579df1c8
letyresinaa <leticiaresina@gmail.com>
2025-09-20 16:01:38 -0300

    fix: metodo de cadastro de cliente

--- src/main/java/com/br/fiap/nexora/controller/ClienteController.java
+++ src/main/java/com/br/fiap/nexora/controller/ClienteController.java
@@ -6,6 +6,8 @@ import com.br.fiap.nexora.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteController {
 
     @Autowired
-    ClienteRepository ClienteRepository;
+    ClienteRepository clienteRepository;
 
     @PostMapping
     @Transactional
-    public void cadastrarCliente(@RequestBody @Valid ClienteDTO clienteDTO){
-        ClienteRepository.save(new Cliente(clienteDTO));
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody @Valid ClienteDTO clienteDTO){
+        Cliente cliente = new Cliente(clienteDTO);
+        clienteRepository.save(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
     }
 }
