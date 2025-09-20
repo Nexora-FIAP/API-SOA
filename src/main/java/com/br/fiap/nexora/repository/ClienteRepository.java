f7bfacc41e71adc59f38a5703b55d53e48449527
letyresinaa <leticiaresina@gmail.com>
2025-09-20 15:58:06 -0300

    fix: repository cliente

--- src/main/java/com/br/fiap/nexora/repository/ClienteRepository.java
+++ src/main/java/com/br/fiap/nexora/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.br.fiap.nexora.repository;
 import com.br.fiap.nexora.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
 }
