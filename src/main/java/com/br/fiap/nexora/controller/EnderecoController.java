fb5659f06f68b5f185d729450d98466dd9fd5c23
letyresinaa <leticiaresina@gmail.com>
2025-09-20 16:02:48 -0300

    feat: repository endereco

---
+++ src/main/java/com/br/fiap/nexora/controller/EnderecoController.java
@@ -0,0 +1,4 @@
+package com.br.fiap.nexora.controller;
+
+public class EnderecoController {
+}
---
+++ src/main/java/com/br/fiap/nexora/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.br.fiap.nexora.repository;
+
+import com.br.fiap.nexora.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+}
